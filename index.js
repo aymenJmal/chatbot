0e954ca8b51c904cf242f35c6a67b335066c894c
Aymen Jmal <aymen.jmal@outlook.com>
2016-12-03 15:35:03 +0100

    updated pages structure

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ let tools = require('./utils');
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 
-let tokens = {};
-tokens['513774525497834'] = 'EAAE5KK8ajkQBAA5KEgv5wP0EeZAZAhm6YWHmZA2m1zzCwmP2sNENFqrzAx7LMArB5A55YNw2csdbBw4aySHEeoTtzym5k2ZCIOU8RDc7tVbfRBiHZC1l04mEfMZAlJOUDMZCSDkJLG0nU5ciEe76SExWLzzQ4nCzp3Rog0E9f7xNgZDZD';
-tokens['493246800721372'] = 'EAAE5KK8ajkQBAF2JEqfuIBMZCJ8QJGlCh0aBvw4qT76V0uCBmagxs8dXefd7puvWYSrouCgWKmY70ZBhfdtKxauWgsAm7p1MOEcCv346HkEiClKhz6CIUEdZCEGDxI1hi9vWJaW7oBuROUNIZA3XsY44JnOoZBMHJdmqLCf0xmQZDZD';
-
-let pages_names = {};
-pages_names['513774525497834'] = 'IpponLab';
-pages_names['493246800721372'] = 'UTOF';
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
@@ -53,9 +46,6 @@ app.post('/webhook/', function (req, res) {
       tools.sendTextMessage(PAGE_ID, SENDER_ID, 'SENDER_ID: ' + SENDER_ID);
       tools.sendTextMessage(PAGE_ID, SENDER_ID, 'TEXT: ' + text.substring(0, 200));
       tools.sendGenericMessage(PAGE_ID, SENDER_ID);
-    } else {
-      tools.sendTextMessage(PAGE_ID, SENDER_ID, 'messaging_events ' + JSON.stringify(messaging_events));
-      break;
     }
   }
   res.sendStatus(200)
